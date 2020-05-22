149f3ff8ce97667b00d1f63d7f5f28c7b3a52fba
王嵩 <31701018@stu.zucc.edu.cn>
2020-05-22 11:03:42 +0800

    first commit

---
+++ src/main/java/cn/edu/zucc/ws/ding/DingApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.zucc.ws.ding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DingApplication.class, args);
+    }
+
+}
